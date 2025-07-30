80f342cac2caa9572ea824214c9b59c93dcd347c
Mana277 <vagjswmm@gmail.com>
2025-07-30 23:05:18 +0900

    Change sample system

--- src/code/include/Render.hpp
+++ src/code/include/Render.hpp
@@ -15,8 +15,9 @@ inline Image<double> Render(int image_width, int image_height, int channnels, co
     // Constants
     const double infinity = std::numeric_limits<double>::infinity();
     const Interval ray_t(0.0001, infinity);
-    const int samples = 1000;
+    const int samples = 100;
     const int Max_depth = 50;
+    const int samples_per_axis = static_cast<int>(sqrt(samples));
     
 
     // Class to store pixel color information (normalized)
@@ -28,16 +29,19 @@ inline Image<double> Render(int image_width, int image_height, int channnels, co
         for (int x = 0; x < image_width; x++) {
             Color color(0,0,0);
             
-            for(int i=0; i < samples; i++){
-                double ram_h = random(0,1.0);
-                double ram_v = random(0,1.0);
-                // Generate ray
-                double h = (x+ram_h) / (image_width-1);
-                double v = (y+ram_v) / (image_height-1);
-                Ray target = cam.getRay(h,v);
+            for (int j = 0; j < samples_per_axis; ++j) {
+                for (int i = 0; i < samples_per_axis; ++i) {
+                    double ram_h = (static_cast<double>(i) + random(0, 1.0)) / samples_per_axis;
+                    double ram_v = (static_cast<double>(j) + random(0, 1.0)) / samples_per_axis;
+                    
+                    // Generate ray
+                    double h = (x+ram_h) / (image_width-1);
+                    double v = (y+ram_v) / (image_height-1);
+                    Ray target = cam.getRay(h,v);
 
-                // calculate color
-                color = color + Caluculate_color(target, cam, world, Max_depth);
+                    // calculate color
+                    color = color + Caluculate_color(target, cam, world, Max_depth);
+                }
             }
             color = color / samples;
             color = GammaCorrection(color, 2.0);
